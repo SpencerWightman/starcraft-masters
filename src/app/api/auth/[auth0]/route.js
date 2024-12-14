faa0f5d1875c14797bd576f1eb7af7b083c15cb7
Spencer Wightman <spencer.wightman@gmail.com>
2024-12-14 13:26:18 +1300

    fixup

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -3,7 +3,11 @@ import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
 export const GET = handleAuth({
   onLogin: async (req, res) => {
     const currentUrl = req.headers.referer || "/";
-    await handleLogin(req, res, { returnTo: currentUrl });
+    await handleLogin(req, res, {
+      authorizationParams: {
+        state: JSON.stringify({ returnTo: currentUrl }),
+      },
+    });
   },
   onLogout: async (req, res) => {
     const currentUrl = req.headers.referer || "/";
