b7374d5618406bc421492f897c664246b702781f
Spencer Wightman <spencer.wightman@gmail.com>
2024-12-18 22:15:00 +1300

    fixup

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest } from "next/server";
 
 export const GET = async (
   req: NextRequest,
-  context: { params: { auth0: string[] } }
+  context: { params: Record<string, string | string[]> }
 ) => {
   const params = await context.params;
   return handleAuth()(req, { params });
@@ -11,7 +11,7 @@ export const GET = async (
 
 export const POST = async (
   req: NextRequest,
-  context: { params: { auth0: string[] } }
+  context: { params: Record<string, string | string[]> }
 ) => {
   const params = await context.params;
   return handleAuth()(req, { params });
