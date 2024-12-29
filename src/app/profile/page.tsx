28a9c017b691e7ea16499134cf836e6f31363eee
Spencer Wightman <spencer.wightman@gmail.com>
2024-12-29 19:47:39 +1300

    unused import

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Paper, Typography, Box, TextField, Button, Fade } from "@mui/material";
 import { useSession, signIn, signOut } from "next-auth/react";
 
