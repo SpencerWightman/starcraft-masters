c6fafcf940135ec4cc9a3762b4232cc60cc90e1d
Spencer Wightman <spencer.wightman@gmail.com>
2025-01-11 23:11:21 +1300

    site metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import WelcomePage from "@/app/welcome/page";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Brood War League",
+  description: "An ASL/SSL fantasy league with player analytics.",
+};
 
 const HomePage: React.FC = () => {
   return <WelcomePage />;
