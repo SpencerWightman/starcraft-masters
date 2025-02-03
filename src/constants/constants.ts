c013dfce10a7192cb959c8b1c8137058804a5228
Spencer Wightman <spencer.wightman@gmail.com>
2025-02-03 18:16:08 +1300

    update deadline

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const deadlineDate = new Date("2025-02-03T23:59:59Z");
+export const deadlineDate = new Date("2025-02-07T23:59:59Z");
 export const mapsNotificationSeason = "19";
 export const leaderboardSeason = "19";
