7f2b015acf89af6e93c5e2dd1850bc8600d15275
Spencer Wightman <spencer.wightman@gmail.com>
2025-05-13 18:02:10 +1200

    update active players

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,10 +1,4 @@
 export const deadlineDate = new Date("2025-03-07T00:00:00Z");
 export const mapsNotificationSeason = "19";
 export const leaderboardSeason = "19";
-export const season19QualifiedPlayers = [
-  "SoulKey",
-  "Snow",
-  "Best",
-  "Skey",
-  "Light",
-];
+export const season19QualifiedPlayers = ["SoulKey", "Snow", "Best"];
