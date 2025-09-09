94e46c739046493bbc3ce404cdfc37efa34ad53e
Spencer Wightman <spencer.wightman@gmail.com>
2025-09-09 12:06:00 +1200

    update active players

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,7 +3,6 @@ export const mapsNotificationSeason = "20";
 export const leaderboardSeason = "20";
 export const currentSeasonName = "ASL Summer 2025";
 export const season20QualifiedPlayers = [
-  "SoulKey",
   "Best",
   "Light",
   "Snow",
@@ -16,7 +15,6 @@ export const season20QualifiedPlayers = [
   "Mini",
   "Sharp",
   "Speed",
-  "Rush",
   "Soma",
   "BTS",
 ];
