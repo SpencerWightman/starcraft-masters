5c7f79a09e64756bbc2b19742c9c06e01f853b6a
Spencer Wightman <spencer.wightman@gmail.com>
2026-02-12 18:59:25 +1300

    update deadline

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const deadlineDate = new Date("2026-03-22T00:00:00Z");
+export const deadlineDate = new Date("2026-02-20T00:00:00Z");
 export const mapsNotificationSeason = "21";
 export const leaderboardSeason = "21";
 export const currentSeasonName = "ASL Season 21";
