2da40b69dd5472c41304bc39a0b37e33e40d8012
Spencer Wightman <spencer.wightman@gmail.com>
2026-02-21 20:46:45 +1300

    qualified player

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,4 @@ export const deadlineDate = new Date("2026-02-20T12:00:00Z");
 export const mapsNotificationSeason = "21";
 export const leaderboardSeason = "21";
 export const currentSeasonName = "ASL Season 21";
-export const season21QualifiedPlayers: string[] = [];
+export const season21QualifiedPlayers: string[] = ["Best"];
